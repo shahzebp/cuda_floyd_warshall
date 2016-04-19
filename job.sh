2c943b0936878e08f358ec466e1f1c309900484b
Akshit Poddar <tg833245@login1.stampede.tacc.utexas.edu>
2016-04-19 16:36:25 -0500

    bloop

--- job.sh
+++ job.sh
@@ -2,10 +2,10 @@
 #SBATCH -J Test        # Job Name
 #SBATCH -o Test.o%j    # Output and error file name (%j expands to jobID)
 #SBATCH -n 16           # Total number of  tasks requested
-#SBATCH -p gpu  # Queue (partition) name -- normal, development, etc.
+#SBATCH -p gpudev  # Queue (partition) name -- normal, development, etc.
 #SBATCH -t 01:00:00     # Run time (hh:mm:ss) - 1.5 hours
 
-N_VERTICES=512 ./a.out
+N_VERTICES=32 ./a.out
 
 #N_VERTICES=$1 ./fwr1 > a_fwr1_output
 #N_VERTICES=$1 ./fwr2 > a_fwr2_output
