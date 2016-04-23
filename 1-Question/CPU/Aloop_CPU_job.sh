4abf5ce02d040a66454e24111baa5c116b92faa0
Aadarsh Jajodia <tg833240@login4.stampede.tacc.utexas.edu>
2016-04-23 18:46:19 -0500

    Adding Files

---
+++ 1-Question/CPU/Aloop_CPU_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J Test        # Job Name
+#SBATCH -o Test.o%j    # Output and error file name (%j expands to jobID)
+#SBATCH -n 16           # Total number of  tasks requested
+#SBATCH -p normal # Queue (partition) name -- normal, development, etc.
+#SBATCH -t 01:00:00     # Run time (hh:mm:ss) - 1.5 hours
+
+m=`echo "2^13" | bc`
+for (( c = 16; c <= $m; c = c * 2))
+do
+        export N_VERTICES=$c
+        echo -n "N_VERTICES = " $c " " >> $1
+        ./Aloop_CPU >> $1
+done
