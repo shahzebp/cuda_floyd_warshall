2bb56fa47c06e59fa36abcd12ecab1aec7cfdf6b
Aadarsh Jajodia <tg833240@login4.stampede.tacc.utexas.edu>
2016-04-23 17:51:20 -0500

    Makin Changes

--- 1-Question/CPU/Dloop_CPU_job.sh
+++ 1-Question/CPU/Dloop_CPU_job.sh
@@ -2,14 +2,13 @@
 #SBATCH -J Test        # Job Name
 #SBATCH -o Test.o%j    # Output and error file name (%j expands to jobID)
 #SBATCH -n 16           # Total number of  tasks requested
-#SBATCH -p gpudev  # Queue (partition) name -- normal, development, etc.
+#SBATCH -p development  # Queue (partition) name -- normal, development, etc.
 #SBATCH -t 01:00:00     # Run time (hh:mm:ss) - 1.5 hours
 
 m=`echo "2^13" | bc`
 for (( c = 16; c <= $m; c = c * 2))
 do
-        export N_THREADS=1024
         export N_VERTICES=$c
-        echo -n "N_THREADS =" $N_THREADS " N_VERTICES = " $c " " >> $1
+        echo -n "N_VERTICES = " $c " " >> $1
         ./Dloop_CPU >> $1
 done
