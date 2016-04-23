dfaefd6429dae81c120fd25a9e2ac0056fc16759
Aadarsh Jajodia <tg833240@login4.stampede.tacc.utexas.edu>
2016-04-23 17:06:03 -0500

    Adding CPUT times

--- 1-Question/CPU/Dloop_CPU_job.sh
+++ 1-Question/CPU/Dloop_CPU_job.sh
@@ -5,4 +5,11 @@
 #SBATCH -p gpudev  # Queue (partition) name -- normal, development, etc.
 #SBATCH -t 01:00:00     # Run time (hh:mm:ss) - 1.5 hours
 
-N_THREADS=1024 N_VERTICES=2048 ./a.out > $1
+m=`echo "2^13" | bc`
+for (( c = 16; c <= $m; c = c * 2))
+do
+        export N_THREADS=1024
+        export N_VERTICES=$c
+        echo -n "N_THREADS =" $N_THREADS " N_VERTICES = " $c " " >> $1
+        ./Dloop_CPU >> $1
+done
