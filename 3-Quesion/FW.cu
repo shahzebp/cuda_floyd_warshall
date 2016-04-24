695e1b73e928dd18b13a02a52571a1dc5c61f390
shahzebp <shahpatel@cs.stonybrook.edu>
2016-04-23 19:40:11 -0500

    3

--- 3-Quesion/FW.cu
+++ 3-Quesion/FW.cu
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<iostream>
 #include <cstdlib>
+#include <sys/time.h>
 #include<limits.h>
 #include<algorithm>
 
@@ -9,80 +10,74 @@ using namespace std;
 #define maxVertices   8192
 #define INF           INT_MAX-1
 #define NS            64
+#define THREADSPB     1024
 
-float dist[maxVertices * maxVertices];
-float *device_matrix;
-float *result_matrix;
+float   dist[maxVertices * maxVertices];
+float   *device_matrix;
+float   *result_matrix;
 
-int vertices;
-int tilesize[3];
-size_t tot;
+int     vertices;
+int     tilesize[3];
+size_t  tot;
 
 __global__
 void FloydWarshall(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, float *matrix, int n, int na)
 {
     int j = blockIdx.x * blockDim.x + threadIdx.x + Xj;
 
-    if (j >= na)
-        return;
-
     int i = blockIdx.y * blockDim.y + threadIdx.y + Xi;
 
-    if (i >= na)
+    if (j >= na || (i >= na))
         return;
 
-    __shared__ long inter;
+    __shared__ long thisrowkthcolumn;
 
-    if (n <= NS) {
+    if (n > NS) {
+        for (int via = Vi; via < (Vi + n); via++) {
+            if (threadIdx.x == 0) 
+                thisrowkthcolumn = matrix[i * na + via];
+            
+            __syncthreads();
+
+            if (i != j && i != via && j != via)
+                matrix[i * na + j] =  min(matrix[i * na + j], 
+                        thisrowkthcolumn + matrix[via * na + j]);
+        }
+    } else {
 
         __shared__ float work[NS];
 
-        work[i * na + j] = matrix[i *na + j];
+        work[i * na + j] = matrix[i * na + j];
 
-        for (int k = Vi; k < (Vi + n); k++) {
-            work[i * na + k] = matrix[i *na + k];
-            work[k * na + j] = matrix[k *na + j];
-         
-
-        __syncthreads();
-
-        for (int k = Vi; k < (Vi + n); k++)  {
-            if (i != j && j != k && i != k) {
-                work[i * na + j] = min(work[i * na + j], work[i * na + k] +
-                        work[k * na + j]);
-            }
-        }
+        for (int via = Vi; via < (Vi + n); via++) {
+            work[i * na + via] = matrix[i *na + via];
+            work[via * na + j] = matrix[via *na + j];
+        }     
 
         __syncthreads();
 
-        for (int k = Vi; k < (Vi + n); k++)  {
-            matrix[i*na + j] = work[i *na + j];
+        for (int via = Vi; via < (Vi + n); via++)  {
+            if (i != j && j != via && i != via)
+                work[i * na + j] = min(work[i * na + j], work[i * na + via] +
+                        work[via * na + j]);
         }
-    } else {
-        for (int k = Vi; k < (Vi + n); k++) {
-            if (threadIdx.x == 0) {
-                inter = matrix[i * na + k];
-            }
-            __syncthreads();
 
-            if (i != j && i != k && j != k)
-                matrix[i * na + j] =  min(matrix[i * na + j], inter + matrix[k * na + j]);
-        }
+        __syncthreads();
+
+        for (int via = Vi; via < (Vi + n); via++)
+            matrix[i * na + j] = work[i * na + j];
     }
 }
 
 void F_loop_FW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n)
 {       
-    int threads_per_block   = 1024;
+    dim3 blocks_per_grid((n + THREADSPB - 1) /
+                                THREADSPB, n);
 
-    dim3 blocks_per_grid((n + threads_per_block - 1) /
-                                threads_per_block, n);
-
-    FloydWarshall<<<blocks_per_grid, threads_per_block>>>(Xi, Xj, Ui,
+    FloydWarshall<<<blocks_per_grid, THREADSPB>>>(Xi, Xj, Ui,
                 Uj, Vi, Vj, device_matrix, n, vertices);           
 
     cudaThreadSynchronize();
-
 }
 
 /*
@@ -115,15 +110,15 @@ void DFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
 		F_loop_FW(Xi, Xj, Ui, Uj, Vi, Vj, n);
 
 	else {
-        for (int k = 0; k < r; k++) {
-            int p = k * (n/r);
+        for (int via = 0; via < r; via++) {
+            int p = via * (n/r);
 
             for (int i = 0; i < r; i++)
                for (int j = 0; j < r; j++) {
                    int ip = i * (n/r);
                    int jp = j * (n/r);
 
-                   if (i != k && j != k)
+                   if (i != via && j != via)
                        DFW(Xi + ip, Xj + jp, Ui + ip, Uj + p, Vi + p, Vj + jp, n/r, d + 1);
                 }
         }
@@ -139,13 +134,13 @@ void BFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
 		F_loop_FW(Xi, Xj, Ui, Uj, Vi, Vj, n);
 
 	else {
-        for (int k = 0; k < r; k++) {
-            int p = k * (n/r);
+        for (int via = 0; via < r; via++) {
+            int p = via * (n/r);
 
             for (int j = 0; j < r; j++) {
                 int ip = j * (n/r);
 
-                if (j != k)
+                if (j != via)
                     BFW(Xi + p, Xj + ip , Ui + p, Uj + p, Vi + p, Vj + ip, n/r, d + 1);
 
             }
@@ -155,7 +150,7 @@ void BFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
                    int ip = i * (n/r);
                    int jp = j * (n/r);
 
-                   if (i != k && j != k)
+                   if (i != via && j != via)
                        DFW(Xi + ip, Xj + jp, Ui + ip, Uj + p, Vi + p, Vj + jp, n/r, d + 1);
                 }
         }
@@ -170,13 +165,13 @@ void CFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
 		F_loop_FW(Xi, Xj, Ui, Uj, Vi, Vj, n);
 
 	else {
-        for (int k = 0; k < r; k++) {
-            int p = k * (n/r);
+        for (int via = 0; via < r; via++) {
+            int p = via * (n/r);
 
             for (int j = 0; j < r; j++) {
                 int ip = j * (n/r);
 
-                if (j != k)
+                if (j != via)
                     CFW(Xi + ip, Xj + p , Ui + ip, Uj + p, Vi + p, Vj + p, n/r, d + 1);
 
             }
@@ -186,7 +181,7 @@ void CFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
                    int ip = i * (n/r);
                    int jp = j * (n/r);
 
-                   if (i != k && j != k)
+                   if (i != via && j != via)
                        DFW(Xi + ip, Xj + jp, Ui + ip, Uj + p, Vi + p, Vj + jp, n/r, d + 1);
                 }
         }
@@ -201,15 +196,15 @@ void AFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
 		F_loop_FW(Xi, Xj, Ui, Uj, Vi, Vj, n);
 
 	else {
-        for (int k = 0; k < r; k++) {
-            int p = k * (n/r);
+        for (int via = 0; via < r; via++) {
+            int p = via * (n/r);
 
             AFW(Xi + p, Xj + p, Ui + p, Uj + p, Vi + p, Vj + p, n/r, d + 1);
 
             for (int j = 0; j < r; j++) {
                 int ip = j * (n/r);
 
-                if (j != k)
+                if (j != via)
                     BFW(Xi + p, Xj + ip , Ui + p, Uj + p, Vi + p, Vj + ip, n/r, d + 1);
 
             }
@@ -217,7 +212,7 @@ void AFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
             for (int j = 0; j < r; j++) {
                 int ip = j * (n/r);
 
-                if (j != k)
+                if (j != via)
                     CFW(Xi + ip, Xj + p , Ui + ip, Uj + p, Vi + p, Vj + p, n/r, d + 1);
 
             }
@@ -227,7 +222,7 @@ void AFW(int Xi, int Xj, int Ui, int Uj, int Vi, int Vj, int n, int d) {
                    int ip = i * (n/r);
                    int jp = j * (n/r);
 
-                   if (i != k && j != k)
+                   if (i != via && j != via)
                        DFW(Xi + ip, Xj + jp, Ui + ip, Uj + p, Vi + p, Vj + jp, n/r, d + 1);
                 }
 
@@ -264,6 +259,8 @@ int main(int argc, char *argv[])
 		}
 	}	
 
+    struct timeval tvalBefore, tvalAfter;
+
     tot = vertices * vertices * sizeof(float);
     device_matrix = NULL;
     cudaMalloc((float **)&device_matrix, tot);
@@ -273,9 +270,18 @@ int main(int argc, char *argv[])
     result_matrix =(float *)malloc( vertices * vertices *
                 sizeof(float));
 
+    gettimeofday (&tvalBefore, NULL);
+
 	AFW(0, 0, 0, 0, 0, 0, vertices, 0);
 
     cudaMemcpy(result_matrix, device_matrix, tot, cudaMemcpyDeviceToHost);
+
+    gettimeofday (&tvalAfter, NULL);
+
+    printf("Time: %ld microseconds\n",
+        ((tvalAfter.tv_sec - tvalBefore.tv_sec)*1000000L
+        +tvalAfter.tv_usec) - tvalBefore.tv_usec
+        );
 
     for(int i = 0 ; i < vertices; i++ ) 
 	{
